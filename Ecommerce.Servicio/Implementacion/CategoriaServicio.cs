1350d01403232dbed4ce2d2a1237cd05aac59afb
lace04 <luisace04@gmail.com>
2023-07-21 12:21:31 -0500

    Vista Categorias

--- Ecommerce.Servicio/Implementacion/CategoriaServicio.cs
+++ Ecommerce.Servicio/Implementacion/CategoriaServicio.cs
@@ -13,119 +13,119 @@ using AutoMapper;
 
 namespace Ecommerce.Servicio.Implementacion
 {
-  public class CategoriaServicio : ICategoriaServicio
-  {
-    private readonly IGenericoRepositorio<Categoria> _modeloRepositorio;
-    private readonly IMapper _mapper;
-
-    public CategoriaServicio(IGenericoRepositorio<Categoria> modeloRepositorio, IMapper mapper)
+    public class CategoriaServicio : ICategoriaServicio
     {
-      _modeloRepositorio = modeloRepositorio;
-      _mapper = mapper;
-    }
+        private readonly IGenericoRepositorio<Categoria> _modeloRepositorio;
+        private readonly IMapper _mapper;
 
-    public async Task<CategoriaDTO> Crear(CategoriaDTO modelo)
-    {
-      try
-      {
-        var dbModelo = _mapper.Map<Categoria>(modelo);
-        var rspModelo = await _modeloRepositorio.Crear(dbModelo);
-
-        if (rspModelo.IdCategoria != 0)
-          return _mapper.Map<CategoriaDTO>(dbModelo);
-        else
-          throw new TaskCanceledException("Categoria no creada");
-      }
-      catch (Exception ex)
-      {
-
-        throw;
-      }
-    }
-
-    public async Task<bool> Editar(CategoriaDTO modelo)
-    {
-      try
-      {
-        var consulta = _modeloRepositorio.Consultar(p => p.IdCategoria == modelo.IdCategoria);
-        var fromDbModelo = await consulta.FirstOrDefaultAsync();
-
-        if (fromDbModelo == null)
+        public CategoriaServicio(IGenericoRepositorio<Categoria> modeloRepositorio, IMapper mapper)
         {
-          fromDbModelo.Nombre = modelo.Nombre;
-          var respuesta = await _modeloRepositorio.Editar(fromDbModelo);
-
-          if (!respuesta)
-            throw new TaskCanceledException("Categoria no editada");
-          return respuesta;
+            _modeloRepositorio = modeloRepositorio;
+            _mapper = mapper;
         }
-        else
+
+        public async Task<CategoriaDTO> Crear(CategoriaDTO modelo)
         {
-          throw new TaskCanceledException("Categoria no encontrada");
+            try
+            {
+                var dbModelo = _mapper.Map<Categoria>(modelo);
+                var rspModelo = await _modeloRepositorio.Crear(dbModelo);
+
+                if (rspModelo.IdCategoria != 0)
+                    return _mapper.Map<CategoriaDTO>(dbModelo);
+                else
+                    throw new TaskCanceledException("Categoria no creada");
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
         }
-      }
-      catch (Exception ex)
-      {
-        throw;
-      }
-    }
 
-    public async Task<bool> Eliminar(int id)
-    {
-      try
-      {
-        var consulta = _modeloRepositorio.Consultar(p => p.IdCategoria == id);
-        var fromDbModelo = await consulta.FirstOrDefaultAsync();
-
-        if (fromDbModelo != null)
+        public async Task<bool> Editar(CategoriaDTO modelo)
         {
-          var respuesta = await _modeloRepositorio.Eliminar(fromDbModelo);
-          if (!respuesta)
-            throw new TaskCanceledException("Categoria no eliminada");
-          else
-            return respuesta;
+            try
+            {
+                var consulta = _modeloRepositorio.Consultar(p => p.IdCategoria == modelo.IdCategoria);
+                var fromDbModelo = await consulta.FirstOrDefaultAsync();
+
+                if (fromDbModelo != null)
+                {
+                    fromDbModelo.Nombre = modelo.Nombre;
+                    var respuesta = await _modeloRepositorio.Editar(fromDbModelo);
+
+                    if (!respuesta)
+                        throw new TaskCanceledException("Usuario no editado");
+                    else
+                        return respuesta;
+                }
+                else
+                    throw new TaskCanceledException("Usuario no encontrado");
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
         }
-        else
-          throw new TaskCanceledException("Categoria no encontrada");
-      }
-      catch (Exception ex)
-      {
-        throw ex;
-      }
+
+        public async Task<bool> Eliminar(int id)
+        {
+            try
+            {
+                var consulta = _modeloRepositorio.Consultar(p => p.IdCategoria == id);
+                var fromDbModelo = await consulta.FirstOrDefaultAsync();
+
+                if (fromDbModelo != null)
+                {
+                    var respuesta = await _modeloRepositorio.Eliminar(fromDbModelo);
+                    if (!respuesta)
+                        throw new TaskCanceledException("Categoria no eliminada");
+                    else
+                        return respuesta;
+                }
+                else
+                    throw new TaskCanceledException("Categoria no encontrada");
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public async Task<List<CategoriaDTO>> Lista(string buscar)
+        {
+            try
+            {
+                var consulta = _modeloRepositorio.Consultar(p => p.Nombre!.ToLower().Contains(buscar.ToLower()));
+
+                List<CategoriaDTO> lista = _mapper.Map<List<CategoriaDTO>>(await consulta.ToListAsync());
+                return lista;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+
+        public async Task<CategoriaDTO> Obtener(int id)
+        {
+            try
+            {
+                var consulta = _modeloRepositorio.Consultar(p => p.IdCategoria == id);
+                var fromDbModelo = await consulta.FirstOrDefaultAsync();
+
+                if (fromDbModelo != null)
+                    return _mapper.Map<CategoriaDTO>(fromDbModelo);
+                else
+                    throw new TaskCanceledException("Categoria no encontrada");
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
     }
-
-    public async Task<List<CategoriaDTO>> Lista(string buscar)
-    {
-      try
-      {
-        var consulta = _modeloRepositorio.Consultar(p => p.Nombre!.ToLower().Contains(buscar.ToLower()));
-
-        List<CategoriaDTO> lista = _mapper.Map<List<CategoriaDTO>>(await consulta.ToListAsync());
-        return lista;
-      }
-      catch (Exception ex)
-      {
-        throw ex;
-      }
-    }
-
-
-    public async Task<CategoriaDTO> Obtener(int id)
-    {
-      try
-      {
-        var consulta = _modeloRepositorio.Consultar(p => p.IdCategoria == id);
-        var fromDbModelo = await consulta.FirstOrDefaultAsync();
-
-        if (fromDbModelo != null)
-          return _mapper.Map<CategoriaDTO>(fromDbModelo);
-        else
-          throw new TaskCanceledException("Categoria no encontrada");
-      }
-      catch (Exception ex)
-      {
-        throw ex;
-      }
-    }
-  }
 }
